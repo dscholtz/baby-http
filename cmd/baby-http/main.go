c034f60411a1954bce46e0a4c8ce367145b19858
Daniel Scholtz <scholtz.daniel97@gmail.com>
2025-06-07 15:17:00 +0200

    add struct handler

--- cmd/baby-http/main.go
+++ cmd/baby-http/main.go
@@ -13,6 +13,12 @@ var helloHandler = handler.HandlerFunc(func(req *request.Request) *request.Respo
 	return request.NewResponse(200, "Hello from baby-http!")
 })
 
+type AboutHandler struct{}
+
+func (h AboutHandler) Serve(req *request.Request) *request.Response {
+	return request.NewResponse(200, "This is a baby-http!!")
+}
+
 // set up the router in main
 func main() {
 	// create a router
@@ -20,14 +26,19 @@ func main() {
 
 	// register a hello route and its hello handler
 	r.Handle("/hello", helloHandler)
+	r.Handle("/about", AboutHandler{})
 
 	// simulate a request to /hello
 	req := request.New("/hello", "GET", "", nil)
+	aboutReq := request.New("/about", "GET", "", nil)
 
 	// pass request to router
 	res := r.Serve(req)
+	aboutRes := r.Serve(aboutReq)
 
 	// print response
 	fmt.Printf("Status: %d\n", res.Status)
 	fmt.Printf("Body: %s\n", res.Body)
+	fmt.Printf("Status: %d\n", aboutRes.Status)
+	fmt.Printf("Body: %s\n", aboutRes.Body)
 }
